7d1d45e83bd958bbd5ec655a8511db3c58362b09
Ismail Ahmad <ismailahmad0505@gmail.com>
2026-02-01 01:11:51 +0500

    added GTM container

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${SourceSans3.variable} ${LeagueSpartan.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5VW4R573');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
