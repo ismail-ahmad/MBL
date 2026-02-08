f9351377889eea4277e7ee9fa0fa4a4ae31b5dc3
Ismail Ahmad <ismailahmad0505@gmail.com>
2026-02-09 00:32:54 +0500

    Remove GAds tag and insert GTM container

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,18 +29,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${SourceSans3.variable} ${LeagueSpartan.variable}`}>
       <head>
-        <script dangerouslySetInnerHTML={{
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
             __html: `
-              <!-- Google tag (gtag.js) -->
-              <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16926495651"></script>
-              <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-16926495651');
-              </script>
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5VW4R573');
             `,
-          }} />
+          }}
+        />
         {/* End Google Tag Manager */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
